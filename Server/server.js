e86689c080c14205e5e2b02e02d259af2a6b0af1
Alvin Okoli <alvinokoli@gmail.com>
2025-05-08 17:08:49 +0100

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(express.json());
 //cors set up
 app.use(cors({
-  origin: 'https://mern-blog-one-ruby.vercel.app/',
+  origin: 'https://mern-blog-one-ruby.vercel.app',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
