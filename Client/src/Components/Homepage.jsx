c11120d554b4f74390889bad180b784e6735b41e
Alvin Okoli <alvinokoli@gmail.com>
2025-05-08 16:53:44 +0100

    Update Homepage.jsx

--- Client/src/Components/Homepage.jsx
+++ Client/src/Components/Homepage.jsx
@@ -11,7 +11,7 @@ export default function Homepage(){
     useEffect(()=>{
         const fetchdata = async ()=>{
             try{
-                const res = await fetch(`http://localhost:3000/?p=${no}`,{
+                const res = await fetch(`https://mern-blog-rx3i.onrender.com/?p=${no}`,{
                     method: 'GET',
                     headers: {
                         'Content-Type': 'application/json'
