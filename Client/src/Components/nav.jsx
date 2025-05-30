602407cb3841b20ac8bb6dae747c942a387b563f
Alvin <alvinokoli@gmail.com>
2025-05-30 02:05:11 +0100

    Added footer

--- Client/src/Components/nav.jsx
+++ Client/src/Components/nav.jsx
@@ -28,38 +28,39 @@ function Nav(){
     return(
         <div className='grid grid-cols-1'>
             
-            <div className='flex justify-center gap-5 mb-3'>
-                <p className='text-5xl lg:text-6xl mt-5 font-bold font-serif'>Afro<span className='text-yellow-300'>log</span></p>
-                <p className='font-normal text-xs mt-12 md:text-sm md:mt-14'>Telling the African story...</p>
-            </div>
+            <div className='flex md:block justify-between px-2'>
+                <NavLink to='/'>
+                    <div className='text-center gap-4 mb-3 md:flex justify-center'>
+                        <p className='text-5xl lg:text-6xl mt-5 font-bold font-serif'>Afro<span className='text-yellow-300'>log</span></p>
+                        <p className='font-normal text-xs md:text-sm md:mt-14'>Telling the African story...</p>
+                    </div>
+                </NavLink>
 
-            <div id='navBg' style={{height: "100px", backgroundImage: `url(${bgImage})`}} className='h-20 '></div>
-            
-            <div id='burger'>
-                <div onClick={handleClick} className='relative flex justify-end cursor-pointer md:hidden'>
+                <div onClick={handleClick} className='flex justify-end cursor-pointer md:hidden'>
                     {show?
-                        <>
-                            <img src="cancel-svgrepo-com.svg" alt="" className='h-12 z-20'/>
-                            <div className='absolute w-full px-auto z-10 py-2 shadow border-b-4 border-b-black bg-white transition transform-fill ease-in-out duration-500 grid grid-cols-1 md:hidden'>
-                            {options.map((option)=>(
-                                <NavOptions option={option} tailwindStyle='mx-2 my-2 font-bold text-xl text-center' key={option.name}/>
-                            ))}
-                        </div> 
-                        </>
+                        <img src="cancel-svgrepo-com.svg" alt="" className='h-12 mt-8'/> 
                     :
-                        <img src="burger-menu-svgrepo-com.svg" alt="" className='h-18'/>
+                        <img src="burger-menu-svgrepo-com.svg" alt="" className='h-12 mt-8'/>
                     }   
 
                 </div> 
-                {/* {show && (
-                    hhhh
-                )} */}
+                
+            </div>
+
+            <div id='navBg' style={{height: "100px", backgroundImage: `url(${bgImage})`}} className='h-20 relative'>
+                    {show&&
+                        <div className='absolute w-full px-auto z-10 pt-2 shadow border-y-4 border-y-black bg-white transition transform-fill ease-in-out duration-500 grid grid-cols-1 md:hidden'>
+                            {options.map((option)=>(
+                                <NavOptions option={option} tailwindStyle='mx-2 my-2 font-bold text-xl text-center border-b-2 border-b-gray-400' key={option.name}/>
+                            ))}
+                        </div>
+                    }
             </div>
 
             <div className='text-center m-3 hidden md:flex flex-wrap justify-center lg:block mt-6 mb-10'>
                 {
                     options.map(option=>(
-                        <NavOptions option={option} tailwindStyle='py-2 px-2 lg:px-4 text-xl rounded-lg hover:bg-yellow-300 hover:text-white' key={option.name}/>
+                        <NavOptions option={option} tailwindStyle='py-2 px-2 lg:px-4 text-xl rounded-lg hover:bg-yellow-300 hover:text-white active:bg-yellow-300  active:text-white' key={option.name}/>
                     ))
                 }
                 
